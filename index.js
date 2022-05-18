e43619a3e3722ec36ff4764988d457abb84470af
navitas2022 <navitas20222021@gmail.com>
2022-05-18 17:40:28 -0400

    index.js edited

--- index.js
+++ index.js
@@ -12,30 +12,30 @@ const app = express();
 
 const ports = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTIONS'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Accept, X-Custom-Header, Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'GET, POST, PUT, DELETE, OPTIONS'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Content-Type, Accept, X-Custom-Header, Authorization'
+//   );
+//   if (req.method === 'OPTIONS') {
+//     return res.status(200).end();
+//   }
+//   next();
+// });
 
-app.use('/auth', authRoutes);
+// app.use('/auth', authRoutes);
 
-app.use('/post', postsRoutes);
+// app.use('/post', postsRoutes);
 
-app.use(errorController.get404);
+// app.use(errorController.get404);
 
-app.use(errorController.get500);
-
+// app.use(errorController.get500);
+console.log("Hello WOrld")
 app.listen(ports, () => console.log(`Listening on port ${ports}`));
